0cc4a9bca4fc3710c69cf241342186db4181500e
wallace123 <wallace123@github.com>
2023-07-20 20:58:33 -0400

    update

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from decouple import config
 openai.organization = config('OPENAI_ORGANIZATION')
 openai.api_key = config('OPENAI_API_KEY')
 
-print(openai.api_key)
+response = openai.ChatCompletion.create(
+    model='gpt-4',
+    messages=[{"role": "user", "content": "Complete the test...france is known for its"}]
+)
+
+print(response.choices[0].message.content)
